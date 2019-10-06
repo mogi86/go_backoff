e1d584915192e5a0097bd98213641ff1f73bbc74
mogi86 <nobuyuki.mogi@gmail.com>
2019-10-06 11:04:25 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mogi86/go_backoff
+
+go 1.12
+
+require rsc.io/quote v1.5.2
---
+++ hello_test.go
@@ -0,0 +1,10 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "こんにちは世界。"
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
